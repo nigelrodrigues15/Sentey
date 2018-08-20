d6ecf664c51ee2aab735e7397b86a0efe2e6faea
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-20 13:06:14 -0700

    nothing works

---
+++ public/javascripts/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const Twitter = require('twitter');
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 
+    // let isbn = '0201558025';
+    // axios.get(`/books/${isbn}`)
+    // .then((response) => {
+    //     console.log(response);
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
+    //
+    // let query = "grace hopper";
+    // axios.get(`/search?string=${query}`)
+    // .then((response) => {
+    //     console.log(response);
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
+
+});
